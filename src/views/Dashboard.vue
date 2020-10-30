9cf0c862f22a1b8a54281801ef69c75fab404c08
MD. Towhidul Islam <tuhintowhidul9@gmail.com>
2020-10-30 11:32:44 +0600

    #17 - Sorting Projects

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -3,6 +3,17 @@
     <h1 class="subheading grey--text">Dashboard</h1>
 
     <v-container class="my-5">
+
+      <v-layout row justify-start class="mb-3">
+        <v-btn small flat color="grey" @click="sortBy('title')">
+          <v-icon small left>folder</v-icon>
+          <span class="caption text-lowercase">By project name</span>
+        </v-btn>
+        <v-btn small flat color="grey" @click="sortBy('person')">
+          <v-icon small left>person</v-icon>
+          <span class="caption text-lowercase">By Person</span>
+        </v-btn>
+      </v-layout>
       
       <v-card flat v-for="project in projects" :key="project.title">
         <v-layout row wrap :class="`pa-3 project ${project.status}`">
@@ -42,6 +53,11 @@ export default {
         { title: 'Design video thumbnails', person: 'Ryu', due: '20th Dec 2018', status: 'complete', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
         { title: 'Create a community forum', person: 'Gouken', due: '20th Oct 2018', status: 'overdue', content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt consequuntur eos eligendi illum minima adipisci deleniti, dicta mollitia enim explicabo fugiat quidem ducimus praesentium voluptates porro molestias non sequi animi!'},
       ]
+    }
+  },
+  methods: {
+    sortBy(prop) {
+      this.projects.sort((a,b) => a[prop] < b[prop] ? -1 : 1)
     }
   }
 }
